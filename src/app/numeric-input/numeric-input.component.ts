a5fd9ebcd4bc9eb174c34bce89cb585ed57cb60b
Cristian <cristian>
2018-07-24 09:48:50 -0500

    local

--- src/app/numeric-input/numeric-input.component.ts
+++ src/app/numeric-input/numeric-input.component.ts
@@ -7,7 +7,7 @@ import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 })
 export class NumericInputComponent implements OnInit {
   @Input() precision: number = 3;
-  @Input() scale: number = 0;
+  @Input() scale: number = 1;
   @Input() placeholder: string = '--,--';
   @Input() pattern: string = '([0-9])|(,)|(arrow)|(space)';
 
@@ -26,25 +26,29 @@ export class NumericInputComponent implements OnInit {
 
   ngOnInit() {
     this.patternValid = new RegExp(this.pattern, 'i');
-    this.patternScale = new RegExp(`[0-9]([0-9]{${this.scale}})$`);
+    console.log('patter', this.patternValid);
+    this.patternScale = new RegExp(`([0-9])([0-9]{${this.scale}})$`);
+    console.log('Scale', this.patternScale);
   }
 
   validateKey(el: any) {
     console.log(el);
-    if (!el.key.match(this.patternValid) ||
-    ( this.previusValue
-      && this.previusValue.length > this.precision - 1
-      && (el.key !== 'Backspace' && el.key !== 'ArrowRight' && el.key !== 'ArrowLeft'))) {
+    if (!el.key.match(this.patternValid) {
       el.preventDefault();
+      console.log('prevent');
     }
   }
 
   ReplaceKey(el) {
+
     this.previusValue = el.target.value.replace(/\D/g, '');
+    console.log('this.prevuisValue', this.previusValue);
     el.target.value = this.scale > 0
       ? this.previusValue.replace(this.patternScale, '$1,$2')
                          .replace(/\B(?=(\d{3})+(?!\d),?)/g, '.')
       : this.previusValue.replace(/\B(?=(\d{3})+(?!\d),?)/g, '.');
+    
+    console.log(el.target);
   }
 }
 
