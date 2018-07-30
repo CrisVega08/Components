be9b52638e6da610df5ed0e76c671baa981f97e0
Cristian <cristian>
2018-07-30 07:25:12 -0500

    local

--- src/app/numeric-input/numeric-input.component.ts
+++ src/app/numeric-input/numeric-input.component.ts
@@ -28,7 +28,6 @@ export class NumericInputComponent implements OnInit {
 
   ngOnInit() {
     this.patternValid = new RegExp(this.pattern, 'i');
-    console.log('patter', this.patternValid);
   }
 
   validateKey(el: any) {
